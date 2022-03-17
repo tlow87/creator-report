91a06586ff22bca05860623bf247b416cbc6a814
Brian Cortright <brcortright@gmail.com>
2022-03-17 16:01:41 -0700

    clean up more starter files, add more styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Hero.css'
 
 const Hero = ({content}) => {
     console.log(content);
     return(
-        <div className="hero-wrapper">
+        <div className="hero-container">
             <h1>{content[0].headline}</h1>
         </div>
     )
