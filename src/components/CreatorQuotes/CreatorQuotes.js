567c5c1bc5ba8cef1197d23fbb68e5b979592222
Brian Cortright <brcortright@gmail.com>
2022-03-21 15:45:41 -0700

    CreatorQuotes updates

--- src/components/CreatorQuotes/CreatorQuotes.js
+++ src/components/CreatorQuotes/CreatorQuotes.js
@@ -3,23 +3,26 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import './CreatorQuotes.css'
 
 const CreatorQuotes = (props) => {
-    console.log(props.quotes);
     return(
-        <div className={`creator_quotes-container ${props.color}`}>
+        <div className={`creator_quotes-container theme-${props.color}`}>
             <div className="base-grid">
                 <h4 className="kelly-green align-center">Words from our creators</h4>
                 {
                     props.quotes.map((item, index) => {
+                        let image = getImage(item.avatar);
+                        console.log(image);
                         return(
                             <div className="quote-wrapper" key={`quote-${index}`}>
-                                <div className="quote">
+                                <div className="f-dir-col f-justify-center quote">
                                     <p dangerouslySetInnerHTML={{ __html: item.quote }}/>
-                                    <a href={`linktr.ee${item.link}`}>linktr.ee<span>{item.link}</span></a>
+                                    <a href={`linktr.ee/${item.link}`}>linktr.ee/<span>{item.link}</span></a>
                                 </div>
-                                <div className="quote-author">
+                                <div className="f-dir-col f-align-center f-justify-center quote-author">
                                     <div className="avatar-wrapper">
-                                        
+                                        <GatsbyImage image={image} alt={item.creator} objectFit="contain" />
                                     </div>
+                                    <p className="p2 kelly-green">{item.creator}</p>
+                                    <p className="p3 kelly-green">{item.followers}</p>
                                 </div>
                             </div>
                         )
